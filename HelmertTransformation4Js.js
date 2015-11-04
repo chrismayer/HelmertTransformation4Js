a95aa63ec757ee4a07f6b230e33e2d2b95cdfab4
Christian Mayer <chris@meggsimum.de>
2015-11-04 10:31:28 +0100

    Restructure main class
    Avoid overwriting the prototype every time an instance of the amin class
    is created.

--- HelmertTransformation4Js.js
+++ HelmertTransformation4Js.js
@@ -9,7 +9,7 @@
  *
  * @author Christian Mayer
  *
- * @version 1.0
+ * @version 1.1-dev
  *
  * @license BSD see license.txt
  *
@@ -49,78 +49,79 @@ function HelmertTransformation4Js(y0, x0, o, a) {
      */
     this.a = a;
 
-    /**
-     * Calculate transformation from origin to destination
-     * as defined in the parameter.
-     *
-     * For example from local grid to national grid.
-     *
-     * @param x a X coordinate in your destination system to be transformed
-     * @param y an Y coordinate in your destination system to be transformed
-     * @param round 'true' if only 3 decimals should be used, default=false
-     *
-     * @returns {Object} Plain JS-object representing the transformed point
-     */
-    HelmertTransformation4Js.prototype.doTransformation = function(x, y, round) {
-
-        var xIn = x;
-        var yIn = y;
-
-        if (typeof round === "undefined" || round === null) {
-            round = false;
-        }
-
-        y = this.y0 + this.o * xIn + this.a * yIn;
-        x = this.x0 + this.a * xIn - this.o * yIn;
-
-        if (round) {
-            y = parseFloat(y.toFixed(3));
-            x =  parseFloat(x.toFixed(3));
-        }
-
-        return {
-            x: x,
-            y: y
-        };
-    };
-
-    /**
-     * Calculate transformation from destination to origin
-     * as defined in the parameter.
-     *
-     * For example from national grid to local grid.
-     *
-     * @param x a X-coordinate in your destination system to be transformed
-     * @param y an Y-coordinate in your destination system to be transformed
-     * @param round 'true' if only 3 decimals should be used, default=false
-     *
-     * @returns {Object} Plain JS-object representing the transformed point
-     */
-    HelmertTransformation4Js.prototype.doReverseTransformation = function(x, y, round) {
-
-        if (typeof round === "undefined" || round === null) {
-            round = false;
-        }
-
-        var aT = this.a / (Math.pow(this.a, 2) + Math.pow(this.o, 2)),
-            oT = this.o / (Math.pow(this.a, 2) + Math.pow(this.o, 2)),
-            y0Rev = -this.x0 * oT + this.y0 * aT,
-            x0Rev = this.x0 * aT + this.y0 * oT,
-            xIn = x,
-            yIn = y;
-
-        y = -y0Rev + aT * yIn - oT * xIn;
-        x = -x0Rev + aT * xIn + oT * yIn;
-
-        if (round) {
-            y = parseFloat(y.toFixed(3));
-            x =  parseFloat(x.toFixed(3));
-        }
-
-        return {
-            x: y,
-            y: x
-        };
-
-    };
 }
+
+/**
+ * Calculate transformation from origin to destination
+ * as defined in the parameter.
+ *
+ * For example from local grid to national grid.
+ *
+ * @param x a X coordinate in your destination system to be transformed
+ * @param y an Y coordinate in your destination system to be transformed
+ * @param round 'true' if only 3 decimals should be used, default=false
+ *
+ * @returns {Object} Plain JS-object representing the transformed point
+ */
+HelmertTransformation4Js.prototype.doTransformation = function(x, y, round) {
+
+    var xIn = x;
+    var yIn = y;
+
+    if (typeof round === "undefined" || round === null) {
+        round = false;
+    }
+
+    y = this.y0 + this.o * xIn + this.a * yIn;
+    x = this.x0 + this.a * xIn - this.o * yIn;
+
+    if (round) {
+        y = parseFloat(y.toFixed(3));
+        x =  parseFloat(x.toFixed(3));
+    }
+
+    return {
+        x: x,
+        y: y
+    };
+};
+
+/**
+ * Calculate transformation from destination to origin
+ * as defined in the parameter.
+ *
+ * For example from national grid to local grid.
+ *
+ * @param x a X-coordinate in your destination system to be transformed
+ * @param y an Y-coordinate in your destination system to be transformed
+ * @param round 'true' if only 3 decimals should be used, default=false
+ *
+ * @returns {Object} Plain JS-object representing the transformed point
+ */
+HelmertTransformation4Js.prototype.doReverseTransformation = function(x, y, round) {
+
+    if (typeof round === "undefined" || round === null) {
+        round = false;
+    }
+
+    var aT = this.a / (Math.pow(this.a, 2) + Math.pow(this.o, 2)),
+        oT = this.o / (Math.pow(this.a, 2) + Math.pow(this.o, 2)),
+        y0Rev = -this.x0 * oT + this.y0 * aT,
+        x0Rev = this.x0 * aT + this.y0 * oT,
+        xIn = x,
+        yIn = y;
+
+    y = -y0Rev + aT * yIn - oT * xIn;
+    x = -x0Rev + aT * xIn + oT * yIn;
+
+    if (round) {
+        y = parseFloat(y.toFixed(3));
+        x =  parseFloat(x.toFixed(3));
+    }
+
+    return {
+        x: y,
+        y: x
+    };
+
+};
